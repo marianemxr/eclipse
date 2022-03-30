d5a52469b0db460722411d6c230bb320cf858673
marianemxr <95103063+marianemxr@users.noreply.github.com>
2022-03-30 03:22:25 -0300

    Adicionei arquivos via upload.

---
+++ Hello GitHub/src/br/com/marianemxr/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.marianemxr.git;
+
+/**
+ * @author Mariane Milene
+ *Exemplo de integração do Eclipse com o GitHub
+*/
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Git!");
+
+	}
+}
